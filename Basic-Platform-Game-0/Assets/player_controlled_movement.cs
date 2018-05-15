8e3de46795043f8ea8f80f677495c031519ac772
Joeyloveswaffles <schmidtj18@icloud.com>
2018-05-15 16:25:56 -0500

    Made Spaceman have input directions that change location
    Spaceman is now affected by inputs such as up down left right which will change the direction the sprite moves.

--- Basic-Platform-Game-0/Assets/player_controlled_movement.cs
+++ Basic-Platform-Game-0/Assets/player_controlled_movement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class player_controlled_movement : MonoBehaviour {
 
-	// Use this for initialization
+	private Rigidbody2D spaceman_rigid_body;
+
 	void Start () {
-		
+		spaceman_rigid_body = GetComponent<Rigidbody2D> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Movement(horizontal, vertical);
+	}
+
+	void Movement(float horizontal, float vertical) {
+		spaceman_rigid_body.velocity = new Vector2 (horizontal * 5, vertical * 5);
 	}
 }
