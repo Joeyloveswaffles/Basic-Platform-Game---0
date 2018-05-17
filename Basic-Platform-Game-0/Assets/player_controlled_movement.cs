290e5ab6ed50369fa2b3a93bce30f710122caedd
Joeyloveswaffles <schmidtj18@icloud.com>
2018-05-16 21:54:01 -0500

    Created Ground, Jumping Physics, Flipped Directions on direction turn
    created the foundations for the game. mostly borrowed code with some changes to personalize it to the individual project.

--- Basic-Platform-Game-0/Assets/player_controlled_movement.cs
+++ Basic-Platform-Game-0/Assets/player_controlled_movement.cs
@@ -6,19 +6,93 @@ public class player_controlled_movement : MonoBehaviour {
 
 	private Rigidbody2D spaceman_rigid_body;
 
-	void Start () {
+	[SerializeField]
+	private float movement_speed;
+
+	[SerializeField]
+	private Transform[] ground_points;
+
+	[SerializeField]
+	private float ground_radius;
+
+	[SerializeField]
+	private LayerMask what_is_ground;
+
+	private bool is_grounded;
+
+	private bool facing_right;
+
+	private bool jump;
+
+	[SerializeField]
+	private float jump_force;
+
+	private void Start () {
 		spaceman_rigid_body = GetComponent<Rigidbody2D> ();
+		facing_right = true;
 	}
 
-	// Update is called once per frame
-	void Update () {
+	private void Update (){
+		handle_input ();
+	}
+		
+	private void FixedUpdate () {
 		float horizontal = Input.GetAxis ("Horizontal");
-		float vertical = Input.GetAxis ("Vertical");
 
-		Movement(horizontal, vertical);
+		is_grounded = is_grounded_check();
+
+		movement(horizontal);
+
+		flip (horizontal);
+
+		reset_values ();
 	}
 
-	void Movement(float horizontal, float vertical) {
-		spaceman_rigid_body.velocity = new Vector2 (horizontal * 5, vertical * 5);
+	private void movement(float horizontal) {
+		spaceman_rigid_body.velocity = new Vector2 (horizontal * movement_speed, spaceman_rigid_body.velocity.y);
+
+		if (is_grounded && jump) {
+			is_grounded = false;
+
+			spaceman_rigid_body.AddForce(new Vector2(0, jump_force)); 	
+		}
+	}
+
+	private void flip(float horizontal) {
+		if ((horizontal > 0 && !facing_right) || (horizontal < 0 && facing_right)) {
+
+			facing_right = !facing_right;
+
+			Vector3 the_scale = transform.localScale;
+
+			the_scale.x *= -1;
+
+			transform.localScale = the_scale;
+		}
+	}
+
+	private void handle_input(){
+		if (Input.GetKey (KeyCode.Space)) {
+			jump = true;
+		}
+	}
+
+	private bool is_grounded_check() {
+		if (spaceman_rigid_body.velocity.y <= 0.05) {
+			foreach (Transform point in ground_points) {
+				Collider2D[] colliders = Physics2D.OverlapCircleAll (point.position, ground_radius, what_is_ground);
+
+				for (int i = 0; i < colliders.Length; i++) {
+					if (colliders [i].gameObject != gameObject) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private void reset_values(){
+		jump = false;
 	}
 }
